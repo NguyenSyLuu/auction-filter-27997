1c6b137f51f7ae1f07f52e8f0a76ba08a8d64760
danghiep92 <dangduyhiep92@gmail.com>
2016-11-01 14:52:47 +0700

    add copyright

--- app/code/local/Magestore/Auction/Block/Adminhtml/Productauction/Renderer/Customeremail.php
+++ app/code/local/Magestore/Auction/Block/Adminhtml/Productauction/Renderer/Customeremail.php
@@ -1,4 +1,28 @@
-<?php 
+<?php
+/**
+ * Magestore
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magestore.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.magestore.com/license-agreement.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Magestore
+ * @package     Magestore_Auction
+ * @module     Auction
+ * @author      Magestore Developer
+ *
+ * @copyright   Copyright (c) 2016 Magestore (http://www.magestore.com/)
+ * @license     http://www.magestore.com/license-agreement.html
+ *
+ */
+
 class Magestore_Auction_Block_Adminhtml_Productauction_Renderer_Customeremail
 	extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
--- app/code/local/Magestore/Auction/Model/System/Template/Noticewinner.php
+++ app/code/local/Magestore/Auction/Model/System/Template/Noticewinner.php
@@ -1,6 +1,29 @@
 <?php
+/**
+ * Magestore
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magestore.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.magestore.com/license-agreement.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Magestore
+ * @package     Magestore_Auction
+ * @module     Auction
+ * @author      Magestore Developer
+ *
+ * @copyright   Copyright (c) 2016 Magestore (http://www.magestore.com/)
+ * @license     http://www.magestore.com/license-agreement.html
+ *
+ */
 
-class Magestore_Auction_Model_System_Template_Noticewinner 
+class Magestore_Auction_Model_System_Template_Noticewinner
 {
     public function toOptionArray()
     {
